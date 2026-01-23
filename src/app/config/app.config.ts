9d00f60bc0f2c7f2b42b12b2f1d1a8a0a8bdfa23
Diana Kozachenko <kozachenkodiana03@mail.ru>
2026-01-23 22:34:11 +0100

    chore: add NgZorro

--- src/app/config/app.config.ts
+++ src/app/config/app.config.ts
@@ -1,7 +1,12 @@
+import { registerLocaleData } from '@angular/common';
+import ru from '@angular/common/locales/ru';
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { ru_RU, provideNzI18n } from 'ng-zorro-antd/i18n';
 import { routes } from '../routes/app.routes';
 
+registerLocaleData(ru);
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes)],
+  providers: [provideBrowserGlobalErrorListeners(), provideRouter(routes), provideNzI18n(ru_RU)],
 };
--- src/app/ui/app.ts
+++ src/app/ui/app.ts
@@ -1,9 +1,11 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
+  imports: [NzButtonComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class App {}
