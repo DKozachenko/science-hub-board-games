be95f0d00baf827dc33a6e773c82680e7a5b6111
Diana Kozachenko <kozachenkodiana03@mail.ru>
2026-03-01 12:52:11 +0100

    feat(pages): add filter options for name column

--- src/pages/main/data/main-columns.constant.ts
+++ src/pages/main/data/main-columns.constant.ts
@@ -1,5 +1,11 @@
 import { Column } from '../model/column.interface';
 import { PlayerRow } from '../model/player-row.interface';
+import { PLAYER_DATA } from './players-data.constant';
+
+const PLAYERS_NAMES_FOR_FILTER: { text: string; value: number }[] = PLAYER_DATA.map((playerData) => ({
+  text: playerData.player.name,
+  value: playerData.player.id,
+}));
 
 export const MAIN_COLUMNS: Column[] = [
   {
@@ -11,32 +17,29 @@ export const MAIN_COLUMNS: Column[] = [
     sortFn: (a: PlayerRow, b: PlayerRow) => a.player.name.localeCompare(b.player.name),
     showFilter: true,
     filterMultiple: true,
-    listOfFilter: [
-      { text: 'Joe', value: 'Joe' },
-      { text: 'Jim', value: 'Jim' },
-    ],
-    filterFn: (list: string[], row: PlayerRow) => list.some((name) => row.player.name.indexOf(name) !== -1),
+    listOfFilter: PLAYERS_NAMES_FOR_FILTER,
+    filterFn: (filterValues: number[], row: PlayerRow) => filterValues.some((id) => row.player.id === id),
   },
   {
-    name: 'Кол-во тренировок',
+    name: 'Тренировки',
     width: '30%',
     showSort: true,
     sortDirections: ['ascend', 'descend', null],
     sortOrder: null,
     sortFn: (a: PlayerRow, b: PlayerRow) => a.totalTrainings - b.totalTrainings,
-    showFilter: true,
+    showFilter: false,
     filterMultiple: true,
     listOfFilter: [],
     filterFn: null,
   },
   {
-    name: 'Кол-во голов',
+    name: 'Голы',
     width: '30%',
     showSort: true,
     sortDirections: ['ascend', 'descend', null],
     sortOrder: null,
     sortFn: (a: PlayerRow, b: PlayerRow) => a.totalGoals - b.totalGoals,
-    showFilter: true,
+    showFilter: false,
     filterMultiple: true,
     listOfFilter: [],
     filterFn: null,
